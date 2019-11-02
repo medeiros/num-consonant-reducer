b350e74081b3ef6e434ceb76ef1b22ea9005f400
Daniel Medeiros <daniel@arneam.com>
2019-11-02 10:01:07 -0300

    Add Lo4j2/Kafka integration

--- src/main/java/com/arneam/numconsonantreducer/application/ConsonantReducerController.java
+++ src/main/java/com/arneam/numconsonantreducer/application/ConsonantReducerController.java
@@ -2,12 +2,14 @@ package com.arneam.numconsonantreducer.application;
 
 import com.arneam.numconsonantreducer.domain.ConsonantReducer;
 import com.arneam.numconsonantreducer.infrastructure.TextReducerClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("reduce")
+@Slf4j
 public class ConsonantReducerController {
 
   @Autowired
@@ -16,6 +18,7 @@ public class ConsonantReducerController {
   @GetMapping("/{name}")
   @ResponseStatus(HttpStatus.OK)
   public int reduceToNumber(@PathVariable String name) {
+    log.info("calling 'ConsonantReducerController#reduceToNumber' with parameter: " + name);
     return textReducerClient.reduceToNumber(ConsonantReducer.instance().reduceToConsonants(name));
   }
 
